49a4cb5415998365d90ba9d8dbb3879c94b8e4cb
Rafael Barbiero <rafael.barbiero@beblue.com.br>
2019-10-15 20:18:16 -0300

    Configure dependencies

---
+++ src/main/java/br/com/rbarbiero/springbatchexercise/SpringBatchExerciseApplication.java
@@ -0,0 +1,13 @@
+package br.com.rbarbiero.springbatchexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchExerciseApplication.class, args);
+	}
+
+}
